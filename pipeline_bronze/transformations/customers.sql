5fbe18709e9ca68058caa7d342b7aa0ddb38a997
thiagovdferraz <thiagovdferraz@gmail.com>
2025-11-11 01:14:32 +0000

    pipeline bronze codes

--- pipeline_bronze/transformations/customers.sql
+++ pipeline_bronze/transformations/customers.sql
@@ -4,4 +4,5 @@ AS
 SELECT
   *,
   current_timestamp() AS ingestion_ts_utc
-FROM STREAM(lakehouse.postgres_public.customers);
+FROM STREAM(lakehouse.postgres_public.customers
+);
--- pipeline_bronze/transformations/yfinance_bronze.sql
+++ pipeline_bronze/transformations/yfinance_bronze.sql
@@ -5,5 +5,18 @@ SELECT
   *
 FROM cloud_files(
   '/Volumes/lakehouse/raw_public/yfinance/commodities/latest_prices/',
-  'json'
+  'json',
+    map(
+    -- Ingestão incremental:
+    -- Se "false", o DLT vai processar apenas os novos arquivos
+    -- que chegarem após o pipeline começar.
+    'cloudFiles.includeExistingFiles', 'false',
+
+    -- Detecta automaticamente o tipo das colunas (útil em JSON)
+    'cloudFiles.inferColumnTypes', 'true',
+
+    -- Permite adicionar novas colunas automaticamente (tem também o rescue)
+    -- se o schema do JSON mudar no futuro
+    'cloudFiles.schemaEvolutionMode', 'addNewColumns'
+  )
 );
